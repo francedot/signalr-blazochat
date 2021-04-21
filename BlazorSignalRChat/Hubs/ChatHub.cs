d4067c8322e568c2122fad3855de3be8fdae59ae
Francesco Bonacci <fbonacci@microsoft.com>
2021-04-21 18:15:16 +0200

    Initial commit

---
+++ BlazorSignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using BlazorSignalRChat.Models;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace BlazorSignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageRaw(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendMessage(ChatMessage message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
---
+++ BlazorSignalRChat/Models/ChatMessage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSignalRChat.Models
+{
+    public class ChatMessage
+    {
+        public ChatMessage()
+        {
+        }
+
+        public ChatMessage(string name, string message)
+        {
+            Name = name;
+            Message = message;
+        }
+
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public string Date { get; set; }
+    }
+}
